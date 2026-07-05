26220332512de6aa906649c3dabe18244683869e
Araame <aram171002@gmail.com>
2026-07-05 19:54:05 +0000

    ProductViewSet and WarehouseViewSet done

--- ecostock/views.py
+++ ecostock/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from ecostock.serializers import ProductSerializer, WarehouseSerializer
+from ecostock.models import Product, Warehouse
 
 # Create your views here.
+class ProductViewSet(ModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
+    
+
+class WarehouseViewSet(ModelViewSet):
+    serializer_class = WarehouseSerializer
+
+
+    def get_queryset(self):
+        return Warehouse.objects.all()
