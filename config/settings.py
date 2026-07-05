7428a326bc6ac30af662aab631465482c2c84722
Araame <aram171002@gmail.com>
2026-07-05 16:44:43 +0000

    Models Warehouse and product done

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecostock',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- ecostock/models.py
+++ ecostock/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Warehouse(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    title = models.CharField(max_length=25, verbose_name="Warehouse'")
+    location = models.CharField(max_length=30, verbose_name="Warehouse's location")
+    capacity = models.IntegerField(verbose_name="Warehouse's capacity")
+
+class Product(models.Model):
+
+    class Status(models.TextChoices):
+        Avalaible = 'avalaible',
+        Perime = 'perime',
+        reserved = 'reserved'
+
+
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    name = models.CharField(max_length=30, verbose_name="Product's name")
+    quantity = models.IntegerField(verbose_name="Quantity")
+    description = models.CharField(max_length=50, verbose_name="Description")
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    status = models.CharField(choices = Status, default = Status.Avalaible, verbose_name="Status")
+
+    
+
+   
+
