16fa6bd94abe6c4152bfc1f77d8eeefa38fe8aab
Araame <aram171002@gmail.com>
2026-07-05 19:43:01 +0000

    Warehouse serializer done

--- ecostock/serializers.py
+++ ecostock/serializers.py
@@ -7,13 +7,24 @@ class ProductSerializer(serializers.ModelSerializer):
     model = Product
     fields = ["id", "name", "quantity", "description", "warehouse", "status"]
 
-    def validate_product(self, data):
+    def validate(self, data):
         if data["name"].length > 30:
-            return serializers.ValidationError("The name must contain 30 characters.")
+            raise serializers.ValidationError("The name must contain 30 characters.")
         
         if data["description"].length > 50 :
-            return serializers.ValidationError("The description must contain 50 characters")
+            raise serializers.ValidationError("The description must contain 50 characters")
+        return data
         
-    
+        
 
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    model = Warehouse
+    fields =  ["id", "title", "location", "capacity"]
+
+    def validate(self, data):
+        if Warehouse.objects.filter(title = data["title"]).exists():
+            raise serializers.ValidationError("The warehouse already exists ")
+        return data
 
